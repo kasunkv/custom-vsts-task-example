0bcd54c90e240dcda51fb9da0ea55a798db47b8c
Kasun Kodagoda <kvkrusl@gmail.com>
2017-08-05 10:22:28 +0530

    Completed custom task code.

--- the-kvk-blog-task/the-kvk-blog-task.ts
+++ the-kvk-blog-task/the-kvk-blog-task.ts
@@ -4,7 +4,12 @@ import path = require('path');
 task.setResourcePath(path.join(__dirname, 'task.json'));
 
 async function run() {
-    
+    let firstName: string = task.getInput('FirstName', true);
+    let lastName: string = task.getInput('LastName');
+
+    // Logic for the custom task
+    console.log(`Hello, ${firstName} ${lastName}`);
+    console.info('Custom Task Completed.');
 }
 
 run();
